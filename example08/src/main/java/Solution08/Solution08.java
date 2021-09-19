8c2970286acaf60238c34972b915b7f43eae86da
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-09-19 14:15:49 -0400

    Initial Commit

---
+++ example08/src/main/java/Solution08/Solution08.java
@@ -0,0 +1,12 @@
+package Solution08;
+
+import java.util.Scanner;
+
+public class Solution08 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("How many people?");
+        int ppl = scnr.nextLine();
+    }
+}
