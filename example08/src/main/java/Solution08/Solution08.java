afec1a004208626285e57c38760c1984e3ba9622
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-09-19 14:32:02 -0400

    Initial Commit

--- example08/src/main/java/Solution08/Solution08.java
+++ example08/src/main/java/Solution08/Solution08.java
@@ -7,6 +7,21 @@ public class Solution08 {
         Scanner scnr = new Scanner(System.in);
 
         System.out.println("How many people?");
-        int ppl = scnr.nextLine();
+        int ppl = scnr.nextInt();
+
+        System.out.println("How many pizzas do you have?");
+        int zas = scnr.nextInt();
+
+        System.out.println("How many slices per pizza?");
+        int slices = scnr.nextInt();
+        int tSlices = slices * zas;
+        System.out.println(ppl + " people with " + zas + " pizzas " + "(" + tSlices + " slices)");
+
+        int zasPerPeep = tSlices / ppl;
+
+        int leftover = tSlices % ppl;
+
+        System.out.println("Each person gets " + zasPerPeep + " piece(s) of pizza.");
+        System.out.println("There are " + leftover + " leftover pieces.");
     }
 }
